538e7308b193a674bec9f1cd0400c9890879f1b9
dreamsyntax <dreamsyntax@gmail.com>
2024-04-16 00:18:50 -0700

    Migrate DraftUI from ShadowTHTextEditor, add theme, add required dependencies

---
+++ TranslationMapper.xaml.cs
@@ -0,0 +1,73 @@
+﻿using shadow_retranslation_assistant.Helpers;
+using System.Windows;
+
+namespace shadow_retranslation_assistant
+{
+    /// <summary>
+    /// Interaction logic for TranslationMapper.xaml
+    /// </summary>
+    public partial class TranslationMapper : Window
+    {
+        public TranslationMapper()
+        {
+            InitializeComponent();
+            ThemeHelper.ApplySkin(Skin.Dark);
+            SetGroupBoxBorder(0.1d);
+        }
+
+        private void Button_PreviewJPAudio_SEQ00_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_PreviewJPAudio_SEQ01_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_PreviewJPAudio_SEQ02_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_PreviewENAudio_SEQ00_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_PreviewENAudio_SEQ01_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_PreviewENAudio_SEQ02_Click(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+
+        private void Button_LoadTargetFolder_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Previous_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Next_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SetGroupBoxBorder(double multiplier)
+        {
+            double thickness = 1.0d * multiplier;
+            var value = new Thickness(thickness, thickness, thickness, thickness);
+            GroupBoxEN.BorderThickness = value;
+            GroupBoxTR.BorderThickness = value;
+            GroupBoxJP.BorderThickness = value;
+        }
+    }
+}
